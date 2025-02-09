ceb684c2a6b269752d7b5e759a285180940d7b4b
okoyfoeciov <92897177+okoyfoeciov@users.noreply.github.com>
2025-02-09 22:28:00 +0700

    Update background.js

--- background.js
+++ background.js
@@ -2,8 +2,6 @@ chrome.runtime.onInstalled.addListener(() => {
     chrome.contextMenus.create({
         id: "llmVietnameseTranslator",
         title: "Dịch",
-        //   id: "[slow]llmVietnameseTranslator",
-        //   title: "Dịch (chậm)",
         contexts: ["selection"],
     });
 });
@@ -12,14 +10,7 @@ chrome.contextMenus.onClicked.addListener(async (info, tab) => {
     if (info.menuItemId === "llmVietnameseTranslator") {
         chrome.scripting.executeScript({
             target: { tabId: tab.id },
-            files: ["src/gpt4omini.bundle.js"],
-        });
-    }
-
-    if (info.menuItemId === "[slow]llmVietnameseTranslator") {
-        chrome.scripting.executeScript({
-            target: { tabId: tab.id },
-            files: ["src/gpt4o.bundle.js"],
+            files: ["src/main.bundle.js"],
         });
     }
 });
