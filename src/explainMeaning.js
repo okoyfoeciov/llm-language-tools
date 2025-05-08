1a2a842a51f69870d8619ba08ff7cf1f6ff2bfc7
okoyfoeciov <92897177+okoyfoeciov@users.noreply.github.com>
2025-05-08 21:50:19 +0700

    update

---
+++ src/explainMeaning.js
@@ -0,0 +1,11 @@
+import { show } from "./utils";
+
+if (window.localStorage.getItem("explainMeaningRegistry") === null) {
+    window.localStorage.setItem("explainMeaningRegistry", window.getSelection().toString())
+    setTimeout(() => {
+        window.localStorage.removeItem("explainMeaningRegistry")
+    }, 300000);
+} else {
+    await show(`Explain the meaning of the word/phrase `${window.localStorage.getItem("explainMeaningRegistry")}` in this context (default to linguistic meaning, but if you think it is a jargon or proper noun, explain the meaning of that jargon or proper name too): ${window.getSelection().toString()}`, "explainMeaning", 1000)
+    window.localStorage.removeItem("explainMeaningRegistry")
+}
