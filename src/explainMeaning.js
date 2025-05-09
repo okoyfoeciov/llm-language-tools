2760a7793c00d5460b91d8db33b9d3ffeefab9e8
okoyfoeciov <92897177+okoyfoeciov@users.noreply.github.com>
2025-05-09 07:26:40 +0700

    Update explainMeaning.js

--- src/explainMeaning.js
+++ src/explainMeaning.js
@@ -3,9 +3,6 @@ import { show } from "./utils";
 if (window.getSelection().toString()) {
     if (window.localStorage.getItem("explainMeaningRegistry") === null) {
         window.localStorage.setItem("explainMeaningRegistry", window.getSelection().toString())
-        setTimeout(() => {
-            window.localStorage.removeItem("explainMeaningRegistry")
-        }, 300000);
     } else {
         await show(`Explain the meaning of the word/phrase \`${window.localStorage.getItem("explainMeaningRegistry")}\` in this context (notice that beyond liguistic meaning, the word/phrase could be a jargon or proper name; provide the response concise as possible): ${window.getSelection().toString()}`, "explainMeaning", 1000)
         window.localStorage.removeItem("explainMeaningRegistry")
