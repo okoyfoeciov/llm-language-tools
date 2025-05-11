39fb4d97638428a2227104461ecf82e45a384ba9
okoyfoeciov <92897177+okoyfoeciov@users.noreply.github.com>
2025-05-11 21:53:02 +0700

    Update paraphrase.js

--- src/paraphrase.js
+++ src/paraphrase.js
@@ -1,3 +1,3 @@
 import { show } from "./utils";
 
-await show(`Paraphrase this text in different ways, maximum of 5 ways, prefer formal/journalistic/scienfic/academic/literary tone (choose the tone that you may think it would fit the tone of original content) but don't make it too unnatural; don't use words or phrases that relate to slang, idioms, cultural dialects or are extremely rare); give the answer directly (no extra words) in a numbered list: `, "paraphrase", 999)
+await show(`Paraphrase this text in different ways, maximum of 5 ways, prefer journalistic/literary/poetic tone (choose the tone that you may think it would fit the tone of original content) but don't make it too unnatural; don't use words or phrases that relate to slang, idioms, cultural dialects or are extremely rare); give the answer directly (no extra words) in a numbered list: `, "paraphrase", 999)
