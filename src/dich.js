0ff53488612c25b32e6cf53d9f552f218b6ea97e
okoyfoeciov <92897177+okoyfoeciov@users.noreply.github.com>
2025-05-22 13:27:38 +0700

    Update dich.js

--- src/dich.js
+++ src/dich.js
@@ -1,3 +1,3 @@
 import { show } from "./utils";
 
-await show(`Dịch sang tiếng Việt (chỉ trả lời bằng bản dịch, không đưa thêm bất cứ văn bản nào khác; hãy dịch tự nhiên và thoát nghĩa: `, "dich", 1000)
+await show(`Dịch sang tiếng Việt (chỉ trả lời bằng bản dịch, không đưa thêm bất cứ văn bản nào khác; cố gắng dịch sát với bản gốc): `, "dich", 1000)
