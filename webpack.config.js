6ad2fb071a7eebfd1b7edd71fa714eb0960a489e
okoyfoeciov <92897177+okoyfoeciov@users.noreply.github.com>
2025-05-24 01:28:14 +0700

    update

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,6 @@ const path = require('path');
 module.exports = {
   entry: {
     dich: './src/dich.js', 
-    paraphrase: './src/paraphrase.js', 
     explainMeaning: './src/explainMeaning.js',
   },
   output: {
